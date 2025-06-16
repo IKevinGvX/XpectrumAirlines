d3265dbf0e9816e12f5be3363e0e44881a2a5f79
Robin1420 <robinzon.1420@gmail.com>
2025-06-16 15:39:43 -0500

    estilos del index actualiados

--- obj/Release/net9.0/Xpectrum-Structure.AssemblyInfo.cs
+++ obj/Release/net9.0/Xpectrum-Structure.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xpectrum-Structure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+494cac6f496a5806b19dc0569020c6d7fb3808bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35041b63aed506476ec8b19422d53c22c4984d3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xpectrum-Structure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xpectrum-Structure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
